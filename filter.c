5db580de09bec02236294aa939cbf199e2ac14a8
smart-al <99301960+smart-al@users.noreply.github.com>
2022-10-19 19:07:16 -0700

    Create filter.c

---
+++ filter.c
@@ -0,0 +1,120 @@
+#include "helpers.h"
+#include <math.h>
+#include <string.h>
+
+
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width; column++)
+        {
+            int rgbGray = round((image[row][column].rgbtBlue + image[row][column].rgbtGreen + image[row][column].rgbtRed) / 3.0);
+
+            image[row][column].rgbtBlue = rgbGray;
+            image[row][column].rgbtGreen = rgbGray;
+            image[row][column].rgbtRed = rgbGray;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Iterate over every row of the image
+    for (int i = 0; i < height; i++)
+    {
+        // Iterate over every column of the image
+        for (int j = 0; j < width; j++)
+        {
+            // Get RGB values
+            int red = image[i][j].rgbtRed;
+            int green = image[i][j].rgbtGreen;
+            int blue = image[i][j].rgbtBlue;
+
+            // Get sepia values based on sepia formula
+            
+            int sepiaRed = round(0.393 * red + 0.769 * green + 0.189 * blue);
+            int sepiaGreen = round(0.349 * red + 0.686 * green + 0.168 * blue);
+            int sepiaBlue = round(0.272 * red + 0.534 * green + 0.131 * blue);
+
+
+            // Set each RGB values to their sepia values
+            image[i][j].rgbtRed = fmin(255, sepiaRed);
+            image[i][j].rgbtGreen = fmin(255, sepiaGreen);
+            image[i][j].rgbtBlue = fmin(255, sepiaBlue);
+        }
+    }
+}
+
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE temp;
+    for (int row = 0; row < height; row++)
+    {
+        for (int column = 0; column < width / 2; column++)
+        {
+            temp = image[row][column];
+            image[row][column] = image[row][width - column - 1];
+            image[row][width - column - 1] = temp;
+        }
+    }
+    return;
+}
+
+// Blur picture
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Temporary storage
+    RGBTRIPLE temp[height][width];
+
+    // Copying the image to keep an unaltered version to loop over
+    memcpy(temp, image, sizeof(RGBTRIPLE) * height * width);
+
+    // Iterate over every row of the image
+    for (int i = 0; i < height; i++)
+    {
+        // Iterate over every column of the image
+        for (int j = 0; j < width; j++)
+        {
+
+
+            float average = 0.0;
+            // Initiate RGB values at 0
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+
+            // Iterate over rows around current row
+            for (int k = -1; k <= 1; k++)
+            {
+                // Iterate over columns around current column
+                for (int l = -1; l <= 1; l++)
+                {
+
+                    if (i + k != height &&
+                        i + k != -1 &&
+                        j + l != width &&
+                        j + l != -1)
+                    {
+                        // Update RGB values to the sum both pixels' RGB values
+                        red += temp[i + k][j + l].rgbtRed;
+                        green += temp[i + k][j + l].rgbtGreen;
+                        blue += temp[i + k][j + l].rgbtBlue;
+                        // Increment average by one for one pixel in the sum
+                        average++;
+                    }
+                }
+            }
+            // Set each RGB values to their blurred values
+            image[i][j].rgbtRed = round(red / average);
+            image[i][j].rgbtGreen = round(green / average);
+            image[i][j].rgbtBlue = round(blue / average);
+        }
+    }
+}
